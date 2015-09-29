8f5c814d5d9b1cc1b23d919a1bd1c379187607ca
Valerie Mettler <valeriemettler@gmail.com>
2015-09-28 21:28:48 -0700

    hello world

---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+
+# Use the shotgun gem so you don't have to restart the server everytime you make a change to your Sinatra app
+
+# if the home, or root, URL '/' is requested, using the normal GET HTTP method,
+# display "Hello, World!"
+get '/' do
+  "Hello World!"
+end
